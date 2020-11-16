895cc26e6169f3874313ecb0c260ff4ed8fac312
liamg15 <golding.liam@outlook.com>
2020-11-15 23:49:39 -0800

    Updated license to MIT in DESCRIPTION, Imported packages using usethis::use_package("dplyr")

--- R/NA2x.R
+++ R/NA2x.R
@@ -1,3 +1,40 @@
+
+#' Replace numeric missing values with mean of the column
+#'
+#' @param data a dataframe containing numeric variables (e.g double or integer).No default data selected, must be an argument.
+#' @param verbose the verbose argument indicates the output was successful, by default verbose = FALSE as this function was designed for small data sets. For larger data sets, verbose = TRUE may be useful to know the function is being applied to  large data frames as the function will take longer. If TRUE, returns Processing successful.
+#' @param fun specifies the function to be applied to missing values. By default, fun refers to mean(.x). An alternative function can be specified (fun = f(x)).
+#' @param trim a fraction of outlying observations to be ignored when calculating for the replacement of NA. By default the trim is set to 0.
+#'
+#' @return With or without a specified function (default = mean), NA2x replaces each missing value with the mean of that column.
+#'
+#' @export
+#'
+#'
+#' @examples
+#' ```
+#' # Create a data frame with both numeric and character variables.
+#'df <-tibble(
+#'  first = c("a", NA, "b", "c","d"),
+#'  second = c(NA, 2, NA, 3, 4),
+#'  third = c(10, NA, NA, 5, 6)
+#' )
+#'
+#'
+#' # Input data frame with missing values.
+#'NA2x(df)
+#'
+#'# The output is the same data frame with modified values.
+#'# A tibble: 5 x 3
+#' first     second third
+#' <chr>      <dbl> <dbl>
+#' 1 a          3    10
+#' 2 NA         2     7
+#' 3 b          3     7
+#' 4 c          3     5
+#' 5 d          4     6
+#'```
+
 NA2x <- function(data, verbose = FALSE, fun = mean, trim = 0) {
   {
     if(verbose) message("Processing successful")
